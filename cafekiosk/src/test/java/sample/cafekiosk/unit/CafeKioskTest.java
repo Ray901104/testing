f9d96b35f168a301d69ec5beecbb4f6f8bf01939
ip323 <ip323@naver.com>
2025-02-03 19:38:52 +0900

    테스트 케이스 세분화하기

--- cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -6,6 +6,7 @@ import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CafeKioskTest {
 
@@ -30,6 +31,28 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
         assertThat(cafeKiosk.getBeverages().get(0).getPrice()).isEqualTo(4000);
+    }
+
+    @Test
+    @DisplayName("음료 여러 개 추가 테스트")
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    @DisplayName("음료 여러 개 추가 테스트 - 경계값")
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
     }
 
     @Test
