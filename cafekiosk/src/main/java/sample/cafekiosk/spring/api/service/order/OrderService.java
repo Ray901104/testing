e9f02baa41a8f07e326333d2e07dde1a30f9149c
ip323 <ip323@naver.com>
2025-02-06 21:19:50 +0900

    Business Layer 테스트 (3)  - @Transactional을 테스트 코드에 걸어도 롤백 관련 로그가 남지 않았던 문제 해결 (application.yml)

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,19 +2,25 @@ package sample.cafekiosk.spring.api.service.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequestDto;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponseDto;
 import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class OrderService {
 
@@ -22,17 +28,48 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final StockRepository stockRepository;
+
+    /**
+     * 재고 감소 -> 동시성 고민
+     * optimistic lock / pessimistic lock / ...
+     */
     public OrderResponseDto createOrder(OrderCreateRequestDto orderCreateRequestDto, LocalDateTime registeredDateTime) {
         List<String> productNumbers = orderCreateRequestDto.getProductNumbers();
 
         // Product
-        List<Product> product = findProductsBy(productNumbers);
+        List<Product> products = findProductsBy(productNumbers);
 
-        Order order = Order.create(product, registeredDateTime);
+        deductStockQuantity(products);
+
+        Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         // Order
         return OrderResponseDto.of(savedOrder);
+    }
+
+    private void deductStockQuantity(List<Product> products) {
+        // 재고 차감 체크가 필요한 상품들 filter
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+        // 재고 엔티티 조회
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        Map<String, Stock> stockMap = createStockMapBy(stocks);
+
+        // 상품별 counting
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
+        // 재고 차감 시도
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+            if (stock.isQuantityLessThan(quantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
     }
 
     private List<Product> findProductsBy(List<String> productNumbers) {
@@ -43,5 +80,22 @@ public class OrderService {
         return productNumbers.stream()
                 .map(productMap::get)
                 .collect(Collectors.toList());
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        return products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .toList();
+    }
+
+    private static Map<String, Stock> createStockMapBy(List<Stock> stocks) {
+        return stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, stock -> stock));
+    }
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        return stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(productNumber -> productNumber, Collectors.counting()));
     }
 }
